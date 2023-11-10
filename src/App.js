8ab232a2a3064003cd878b093e922c30cd0172aa
tejasrao712 <tejasrao712@gmail.com>
2023-11-10 15:36:01 +0530

    First error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import HeroSection from './Components/HeroSection';
-import AboutSection from './Components/AboutSection';
+import HeroSection from './Components/Desktop1/HeroSection';
+import AboutSection from './Components/About/AboutSection';
 import ScrollToBottomButton from './Components/ScrollButton';
-import Footer from './Components/Footer';
+import Footer from './Components/Desktop1/Footer';
 import NavBar from './Components/Desktop1/NavBar';
 
 
@@ -11,7 +11,7 @@ import NavBar from './Components/Desktop1/NavBar';
 function App() {
   return (
     <div>
-      
+
       <NavBar />
       <HeroSection/>
       <AboutSection />
--- src/Components/Desktop1/NavBar.js
+++ src/Components/Desktop1/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TechStack from "../TechStack";
 import AboutSection from "../About/AboutSection";
 import myProjects from "../Projects/myProjects";
@@ -8,36 +8,36 @@ import Contact from "../Contact";
 
 
 const NavBar = () => {
-    return(
-        <Router>
+  return (
+    <Router>
 
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>   
-              </li>
-              <li>
-                <Link to="/TechStack">Tech Stack</Link>
-              </li>
-              <li>
-                <Link to="/Projects">Projects</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Route path="/about" exact component={AboutSection} />
-          <Route path="/TechStack" exact component={TechStack} />
-          <Route path="/Projects" exact component={myProjects} />
-          <Route path="/contact" exact component={Contact} />
-
-        </Router>
-    );
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/TechStack">Tech Stack</Link>
+          </li>
+          <li>
+            <Link to="/Projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/about" exact component={AboutSection} />
+        <Route path="/TechStack" exact component={TechStack} />
+        <Route path="/Projects" exact component={myProjects} />
+        <Route path="/contact" exact component={Contact} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default NavBar;
